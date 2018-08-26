85914fd4f118eb092acaff2af452b8d050b7d2e8
Karolis Stulgys <k.stulgys@gmail.com>
2018-08-26 15:25:05 +1000

    removed borders

--- src/battle/PlayerInput.js
+++ src/battle/PlayerInput.js
@@ -1,66 +1,62 @@
-import React, { Component } from "react"
-import {
-	InputLabel,
-	FormControl,
-	Typography,
-	Input,
-	Button
-} from "@material-ui/core"
+import React, { Component } from 'react'
+import { InputLabel, FormControl, Typography, Input, Button } from '@material-ui/core'
 
 class PlayerInput extends Component {
-	state = {
-		username: "",
-		submitted: false
-	}
+  state = {
+    username: '',
+    submitted: false
+  }
 
-	handleChange = ({ target: { value } }) => {
-		this.setState({ username: value })
-	}
+  handleChange = ({ target: { value } }) => {
+    this.setState({ username: value })
+  }
 
-	handleSubmit = e => {
-		e.preventDefault()
-		this.props.onSubmit(this.props.id, this.state.username)
-		this.setState({ submitted: true })
-	}
+  handleSubmit = e => {
+    e.preventDefault()
+    this.props.onSubmit(this.props.id, this.state.username)
+    this.setState({ submitted: true })
+  }
 
-	render() {
-		return (
-			<div style={{ padding: 20 }}>
-				<form onSubmit={this.handleSubmit}>
-					<div
-						style={{
-							display: "flex",
-							flexDirection: "column",
-							alignItems: "center",
-							justifyContent: "space-around",
-							height: 300,
-							width: 300,
-							border: "1px solid red"
-						}}>
-						<Typography variant="title" gutterBottom>
-							{this.props.label}
-						</Typography>
+  render() {
+    return (
+      <div style={{ padding: 20 }}>
+        <form onSubmit={this.handleSubmit}>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'space-around',
+              height: 300,
+              width: 300
+              // border: "1px solid red"
+            }}
+          >
+            <Typography variant="title" gutterBottom>
+              {this.props.label}
+            </Typography>
 
-						<FormControl>
-							<InputLabel>gitHub Username</InputLabel>
-							<Input
-								value={this.state.username}
-								type="text"
-								onChange={this.handleChange}
-							/>
-						</FormControl>
-						<Button
-							variant="contained"
-							color="primary"
-							type="submit"
-							disabled={!this.state.username}>
-							Submit
-						</Button>
-					</div>
-				</form>
-			</div>
-		)
-	}
+            <FormControl>
+              <InputLabel>gitHub Username</InputLabel>
+              <Input
+                value={this.state.username}
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormControl>
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              disabled={!this.state.username}
+            >
+              Submit
+            </Button>
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default PlayerInput
--- src/battle/PlayerPreview.js
+++ src/battle/PlayerPreview.js
@@ -1,33 +1,34 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { Button, Avatar, Typography } from "@material-ui/core"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Button, Avatar, Typography } from '@material-ui/core'
 
 const PlayerPreview = props => {
-	return (
-		<div style={{ padding: 20 }}>
-			<div
-				style={{
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-					justifyContent: "center",
-					height: 300,
-					width: 300,
-					border: "1px solid green"
-				}}>
-				<Avatar
-					alt="Adelle Charles"
-					src={props.avatar}
-					style={{ height: 200, width: 200 }}
-				/>
-				<Typography variant="headline">{props.username}</Typography>
+  return (
+    <div style={{ padding: 20 }}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: 300,
+          width: 300
+          // border: "1px solid green"
+        }}
+      >
+        <Avatar
+          alt="Adelle Charles"
+          src={props.avatar}
+          style={{ height: 200, width: 200 }}
+        />
+        <Typography variant="headline">{props.username}</Typography>
 
-				<Button color="primary" onClick={() => props.onHandleReset(props.id)}>
-					Reset
-				</Button>
-			</div>
-		</div>
-	)
+        <Button color="primary" onClick={() => props.onHandleReset(props.id)}>
+          Reset
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default PlayerPreview
