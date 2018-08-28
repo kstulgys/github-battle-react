d2040718c0f52784c4f4538955fede910dc17997
Karolis Stulgys <k.stulgys@gmail.com>
2018-08-28 16:44:30 +1000

    downgrade material-ui to work with recompose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './app'
-import 'typeface-roboto'
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./app"
+import "typeface-roboto"
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"))
