9c51bd17e35c7a209e44194c05f0ea2aab3292cb
Karolis Stulgys <k.stulgys@gmail.com>
2018-08-29 13:50:20 +1000

    separated impure api call actions in api helpers

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -1,46 +1,63 @@
-import * as R from 'ramda'
+import * as R from "ramda"
 const log = R.tap(console.log)
 
-export const handleError = error => {
-  console.warn(error)
-  return null
+const Impure = {
+	getData: async username => {
+		const getProfile = async username => {
+			const profile = await fetch(`https://api.github.com/users/${username}`)
+			return profile.json()
+		}
+		const getRepos = async username => {
+			const repos = await fetch(
+				`https://api.github.com/users/${username}/repos`
+			)
+			return repos.json()
+		}
+		const [profile, repos] = await Promise.all([
+			getProfile(username),
+			getRepos(username)
+		])
+		return {
+			profile,
+			repos
+		}
+	},
+	getAllProfilesAndRepos: async players => await Promise.all(players),
+	getPopularRepos: async language => {
+		const url = `https://api.github.com/search/repositories?q=>1+language:${language}&sort=stars&order=desc`
+		const response = await fetch(url).catch(handleError)
+		const repos = await response.json()
+		return repos.items
+	}
 }
 
-const getProfile = async username => {
-  const profile = await fetch(`https://api.github.com/users/${username}`)
-  return profile.json()
-}
-const getRepos = async username => {
-  const repos = await fetch(`https://api.github.com/users/${username}/repos`)
-  return repos.json()
-}
-
-const getData = async player => {
-  const [profile, repos] = await Promise.all([getProfile(player), getRepos(player)])
-  return {
-    profile,
-    repos
-  }
-}
-
-const getFolowers = R.path(['profile', 'followers'])
-
+const getFolowers = R.path(["profile", "followers"])
 const getStars = R.pipe(
-  R.prop('repos'),
-  R.map(n => n.stargazers_count),
-  R.reduce(R.add, 0)
+	R.prop("repos"),
+	R.map(n => n.stargazers_count),
+	R.reduce(R.add, 0)
 )
-
 const calcScore = (x, y) => ({
-  score: x * 3 + y
+	score: x * 3 + y
 })
-
-export const getAllProfilesAndRepos = async players =>
-  await Promise.all(R.map(getData, players))
 const getScore = R.converge(calcScore, [getFolowers, getStars])
 const withScore = R.converge(R.merge, [x => getScore(x), x => x])
-export const getAllWithScore = R.pipe(
-  R.map(withScore),
-  R.map(R.omit(['repos']))
-)
-export const getAllSortedByScore = R.sortWith([R.descend(R.prop('score'))])
+
+const data = {
+	getAllWithScore: R.pipe(
+		R.map(withScore),
+		R.map(R.omit(["repos"]))
+	),
+	getAllProfilesAndRepos: R.pipe(
+		R.map(Impure.getData),
+		Impure.getAllProfilesAndRepos
+	),
+	getPopularRepos: Impure.getPopularRepos,
+	handleError: error => {
+		console.warn(error)
+		return null
+	},
+	getAllSortedByScore: R.sortWith([R.descend(R.prop("score"))])
+}
+
+export default data
--- src/api/index.js
+++ src/api/index.js
@@ -1,88 +1,11 @@
-import {
-  getAllProfilesAndRepos,
-  getAllWithScore,
-  getAllSortedByScore,
-  handleError
-} from './helpers'
-import * as R from 'ramda'
+import data from "./helpers"
+import * as R from "ramda"
 const log = R.tap(console.log)
 
 export const sortAllPlayers = R.pipeP(
-  getAllProfilesAndRepos,
-  getAllWithScore,
-  getAllSortedByScore
+	data.getAllProfilesAndRepos,
+	data.getAllWithScore,
+	data.getAllSortedByScore
 )
 
-export const fetchPopularRepos = async language => {
-  const url = `https://api.github.com/search/repositories?q=>1+language:${language}&sort=stars&order=desc`
-  const response = await fetch(url).catch(handleError)
-  const repos = await response.json()
-  return repos.items
-}
-
-// const url = language =>
-// 	`https://api.github.com/search/repositories?q=>1+language:${language}&sort=stars&order=desc`
-
-// export const fetchPopularRepos = R.curry(
-// 	fetch(url)
-// 		.then(resp => resp.json())
-// 		.then(repos => repos.items)
-// )
-
-// const getProfile = async username => {
-// 	const resp = await fetch(`https://api.github.com/users/${username}`)
-// 	return resp.json()
-// }
-
-// const getRepos = async username => {
-// 	const resp = await fetch(`https://api.github.com/users/${username}/repos`)
-// 	return resp.json()
-// }
-
-// const getStarCount = repos => {
-// 	return repos.reduce(
-// 		(count, { stargazers_count }) => count + stargazers_count,
-// 		0
-// 	)
-// }
-
-// const calculateScore = (profile, repos) => {
-// 	const followers = profile.followers
-// 	const totalStars = getStarCount(repos)
-// 	return followers * 3 + totalStars
-// }
-
-// const handleError = error => {
-// 	console.warn(error)
-// 	return null
-// }
-
-// const getUserData = async player => {
-// 	const [profile, repos] = await Promise.all([
-// 		getProfile(player),
-// 		getRepos(player)
-// 	])
-// 	return {
-// 		profile,
-// 		score: calculateScore(profile, repos)
-// 	}
-// }
-
-// const sortPlayers = players => {
-// 	const byScore = R.descend(R.prop("score"))
-// 	return R.sort(byScore, players)
-// }
-
-// export const battle = async players => {
-// 	const playersArray = await Promise.all(players.map(getUserData)).catch(
-// 		handleError
-// 	)
-// 	console.log(playersArray)
-// 	return playersArray === null ? playersArray : sortPlayers(playersArray)
-// }
-// export const fetchPopularRepos = async language => {
-// 	const url = `https://api.github.com/search/repositories?q=>1+language:${language}&sort=stars&order=desc`
-// 	const response = await fetch(url).catch(handleError)
-// 	const repos = await response.json()
-// 	return repos.items
-// }
+export const fetchPopularRepos = data.getPopularRepos
